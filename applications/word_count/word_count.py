eda4b05e1cf8eea7e20adc0904bd6c752eee79a2
Ian <lolsugotpwnd@yahoo.com>
2020-11-05 22:54:35 -0600

    word_count wip

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,12 +1,21 @@
 def word_count(s):
     # Your code here
-    lowerCase = dict((k.lower(), v) for k , v in s .items())
+    counts = dict()
+    words = s.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
     
 
 
 
 if __name__ == "__main__":
     print(word_count(""))
-    print(word_count("Hello"))
+    print(word_count("Hello    hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
